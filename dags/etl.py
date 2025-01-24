b92c4c18153a50412f301a268ceee2e5d99faad5
Arno <arnojooste3008@gmail.com>
2025-01-24 11:16:04 +0200

    Integrated NASA API with Extract pipeline

--- dags/etl.py
+++ dags/etl.py
@@ -34,6 +34,15 @@ with DAG(
         postgres_hook.run(create_table_query)
 
     # Step 2: Extract data from NASA API (i.e. Astronomy Picture Of The Day (APOD) data)
+    # https://api.nasa.gov/planetary/apod?api_key=V5H1G7bkDb5kBF5MgBbsAEPUCu5CGLk7AhABoc90
+    extract_nasa_apod_data = SimpleHttpOperator(
+        task_id="extract_nasa_apod_data",
+        http_conn_id="nasa_api", # connection id defined in airflow for nasa api
+        endpoint="planetary/apod", # nasa api endpoint for APOD
+        method="GET",
+        data={"api_key": "{{ conn.nasa_api.extra_dejson.api_key }}"}, # get api key from connection
+        response_filter=lambda response: response.json(), # convert response to json
+    )
 
 
     # Step 3: Transform the data (i.e. Pick the information that needs to be saved)
